3c550d4f2f53f6748587ab3c069ea79ca9a70cb1
MatheusFranzener <felipe_mielke-vieira@estudante.sc.senai.br>
2022-06-24 21:40:01 -0300

    24/06 - 21:39
    HTML e CSS do componente item

--- Almoxarifado/src/app/app.module.ts
+++ Almoxarifado/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UsersService } from './users.service';
+import { PrincipalModule } from './Principal/principal.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    PrincipalModule
   ],
   providers: [
     UsersService
