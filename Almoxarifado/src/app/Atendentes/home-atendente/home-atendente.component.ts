f1c33055ee564627523ba8a2534b8b0c10a56f53
ThiagoBrag <92130098+ThiagoBrag@users.noreply.github.com>
2022-07-08 21:33:28 -0300

    08/07 - 21:33
    Tela de atendente editada, precisa de finalização ainda

--- Almoxarifado/src/app/Atendentes/home-atendente/home-atendente.component.ts
+++ Almoxarifado/src/app/Atendentes/home-atendente/home-atendente.component.ts
@@ -1,3 +1,4 @@
+import { htmlAstToRender3Ast } from '@angular/compiler/src/render3/r3_template_transform';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAtendenteComponent implements OnInit {
 
-  aparecer: number = 1
+  aparecer: number = 0
+  localizacao: number = 0
+  listaItens: number = 0
+  confirmarRetirada: number = 0
+  baixaDevolucoes: number = 0
 
   constructor() { }
 
@@ -22,5 +27,42 @@ export class HomeAtendenteComponent implements OnInit {
     this.aparecer = 0
   }
 
+  telaLocalizacoes() {
+    this.listaItens = 0
+    this.confirmarRetirada = 0
+    this.baixaDevolucoes = 0
+    this.localizacao = 1
+  }
+
+  telaListaItens() {
+    this.localizacao = 0
+    this.confirmarRetirada = 0
+    this.baixaDevolucoes = 0
+    this.listaItens = 1
+  }
+
+  telaConfirmarRetirada() {
+    this.localizacao = 0
+    this.listaItens = 0
+    this.baixaDevolucoes = 0
+    this.confirmarRetirada = 1
+  }
+
+  telaBaixaDevolucoes() {
+    this.localizacao = 0
+    this.listaItens = 0
+    this.confirmarRetirada = 0
+    this.baixaDevolucoes = 1
+
+      let botaoAzul = document.querySelector('#baixaDevolucoes') as HTMLElement;
+      botaoAzul.style.backgroundColor = '#0047B6';
+      botaoAzul.style.color = '#ffff'
+
+      let botaoSemAzul = document.querySelector('#localizacoes') as HTMLElement;
+      botaoSemAzul.style.backgroundColor = '#ffff';
+      botaoSemAzul.style.color = '#000';
+
+
+  }
 
 }
