b498e778be866e1d37805806fc970ae1e48390b3
Kenzohfs <matheus_hohmann@estudante.sc.senai.br>
2022-07-08 21:23:26 -0300

    08/07 21:23
    Modais e tela de home supervisor

--- Almoxarifado/src/app/Supervisor/home-supervisor/home-supervisor.component.ts
+++ Almoxarifado/src/app/Supervisor/home-supervisor/home-supervisor.component.ts
@@ -12,4 +12,41 @@ export class HomeSupervisorComponent implements OnInit {
   ngOnInit() {
   }
 
+  contadorRecusar = 0;
+  contadorAceitar = 0;
+  contadorUsuarios = 0;
+  contadorCadastros = 0;
+
+  modalRecusar() {
+    this.contadorRecusar = 1;
+    let divPrincipal = document.querySelector('.divPrincipal') as HTMLElement;
+    divPrincipal.style.opacity = '0.5';
+  }
+
+  modalAceitar() {
+    this.contadorAceitar = 1;
+    let divPrincipal = document.querySelector('.divPrincipal') as HTMLElement;
+    divPrincipal.style.opacity = '0.5';
+  }
+
+  cancelar() {
+    this.contadorRecusar = 0;
+    this.contadorAceitar = 0;
+    let divPrincipal = document.querySelector('.divPrincipal') as HTMLElement;
+    divPrincipal.style.opacity = '1';
+  }
+
+  gerenciarUsuarios() {
+    this.contadorUsuarios = 1;
+    let botaoAzul = document.querySelector('#botaoMenu') as HTMLElement;
+    botaoAzul.style.backgroundColor = '#0047B6';
+    botaoAzul.style.color = '#ffff'
+    let botaoSemAzul = document.querySelector('#botaoMenuCor') as HTMLElement;
+    botaoSemAzul.style.backgroundColor = '#ffff';
+    botaoSemAzul.style.color = '#000';
+    let tirarDiv = document.querySelector('.usuarios') as HTMLElement;
+    tirarDiv.remove();
+  }
+
+
 }
