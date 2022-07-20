892199c54131e096b37169172af8b1326940cfe1
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-07-20 19:40:52 -0300

    20/07 - 19:40

--- Almoxarifado/src/app/Principal/reserva/reserva.component.ts
+++ Almoxarifado/src/app/Principal/reserva/reserva.component.ts
@@ -7,32 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ReservaComponent implements OnInit {
 
-  @Input() corStatus: String = "none";
+  @Input() corStatus: string;
 
   constructor() { }
 
   ngOnInit() {
-    //not working
-    const status = document.querySelector("#status") as HTMLElement;
-    console.log("status: ", status)
-    switch (this.corStatus) {
-      case "vermelho":
-        status.style.backgroundColor = "red";
-        break;
-      case "amarelo":
-        status.style.backgroundColor = "yellow";
-        break;
-      case "verde":
-        status.style.backgroundColor = "green";
-        break;
-      default:
-        status.style.backgroundColor = "white";
-        break;
-    }
-  }
-
-  addColor(cor) {
-    console.log(cor);
   }
 
 }
