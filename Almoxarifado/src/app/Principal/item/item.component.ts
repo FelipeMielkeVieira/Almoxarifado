55f5384253cebaa6ad9ec2e3059fa2c7c8b38bda
MatheusFranzener <felipe_mielke-vieira@estudante.sc.senai.br>
2022-07-20 19:57:05 -0300

    20/07 - 19:56
    Correções no modal do produto

--- Almoxarifado/src/app/Principal/item/item.component.ts
+++ Almoxarifado/src/app/Principal/item/item.component.ts
@@ -11,6 +11,12 @@ export class ItemComponent implements OnInit {
 
   textoDescartavel: string;
   aparecer: boolean = false;
+  qtd: number = 1;
+
+  calendarioAberto1: number = 0;
+  calendarioAberto2: number = 0;
+  data1: string = "__/__/____ 00:00";
+  data2: string = "__/__/____ 00:00";
   
   constructor() { }
 
@@ -30,9 +36,41 @@ export class ItemComponent implements OnInit {
     this.aparecer = false;
   }
 
+  mudarQtd(variavel) {
+    if(variavel == 1) {
+      if(this.qtd < this.item.quantidade) {
+        this.qtd++;
+      }
+    } else {
+      if(this.qtd > 1) {
+        this.qtd--;
+      }
+    }
+  }
+
   buscarClassificacao(codigoClassificacao) {
     
     return "Classificação"
+  }
+
+  abrirCalendario(numero) {
+    if(numero == 1) {
+      this.calendarioAberto2 = 0;
+      this.calendarioAberto1 = 1;
+    } else {
+      this.calendarioAberto1 = 0;
+      this.calendarioAberto2 = 1;
+    }
+  }
+
+  salvarData1(data) {
+    this.calendarioAberto1 = 0;
+    this.data1 = data;
+  }
+
+  salvarData2(data) {
+    this.calendarioAberto2 = 0;
+    this.data2 = data;
   }
 
 }
