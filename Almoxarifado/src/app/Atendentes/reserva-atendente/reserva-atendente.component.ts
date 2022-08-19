6bbc5f29221410e03fa9accb032a39a781bbf8cd
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-08-19 19:45:14 -0300

    19/08 - 19:45

--- Almoxarifado/src/app/Atendentes/reserva-atendente/reserva-atendente.component.ts
+++ Almoxarifado/src/app/Atendentes/reserva-atendente/reserva-atendente.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reserva-atendente',
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservaAtendenteComponent implements OnInit {
 
+  @Input() lista;
+
   constructor() { }
 
   ngOnInit() {
+    if (this.lista == 1) {
+      console.log(1);
+    }
   }
+
+
 }
