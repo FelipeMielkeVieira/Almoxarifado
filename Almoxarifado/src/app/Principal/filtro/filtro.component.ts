106bf0fdb6e729f647b5bd8e3d31624820b356ba
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-08-23 21:28:36 -0300

    23/08 - 21:28

--- Almoxarifado/src/app/Principal/filtro/filtro.component.ts
+++ Almoxarifado/src/app/Principal/filtro/filtro.component.ts
@@ -32,7 +32,51 @@ export class FiltroComponent implements OnInit {
     this.filtroSelecionado = item;
     this.fecharFiltro();
     
-    let divFiltro = document.querySelector("");
+    let divFiltro = document.querySelector("#filtro");
+
+    const containerFiltro = document.createElement("div");
+    const textoFiltro = document.createElement("p");
+    const containerExcluir = document.createElement("div");
+    const imgExcluir = document.createElement("img");
+
+    textoFiltro.innerText = item.classificacao;
+    imgExcluir.src = "../../../assets/close.png";
+
+    containerFiltro.id = "containerFiltro";
+    containerExcluir.id = "containerExcluir";
+    imgExcluir.id = "imgExcluir";
+
+    containerFiltro.style.marginTop = "10px"
+    containerFiltro.style.display = "flex";
+    containerFiltro.style.alignItems = "center";
+    containerFiltro.style.padding = "5px"
+    containerFiltro.style.borderRadius = "5px"
+    containerFiltro.style.backgroundColor = "white";
+    containerFiltro.style.border = "1px solid gray";
+    containerFiltro.style.width = "fit-content"
+
+    containerExcluir.style.display = "flex";
+    containerExcluir.style.alignItems = "center";
+    containerExcluir.style.marginLeft = "10px"
+    containerExcluir.style.cursor = "pointer";
+    containerExcluir.onclick = this.tirarFiltro;
+
+    imgExcluir.style.width = "15px"
+
+    containerFiltro.appendChild(textoFiltro);
+    containerFiltro.appendChild(containerExcluir);
+
+    containerExcluir.appendChild(imgExcluir);
+
+    divFiltro.appendChild(containerFiltro);
+  }
+
+  tirarFiltro() {
+    let divFiltro = document.querySelector("#filtro");
+    let containerFiltro = document.querySelector("#containerFiltro");
+
+    divFiltro.removeChild(containerFiltro);
+    this.filtroSelecionado = null;
   }
 
   fecharFiltro() {
