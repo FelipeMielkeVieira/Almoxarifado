b41a9cebd31dab9abbf2009ebb89119f7e2b90a0
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-05 20:29:08 -0300

    05/10 - 20:29
    routing.ts

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: "teste", component: SacolaComponent },
+  // { path: '', component: LoginComponent },
+  // { path: 'cadastro', component: CadastroComponent },
+  // {
+  //   path: 'professor', canActivate: [CheckLogged], children: [
+  //     { path: '', component: HomeProfessorComponent },
+  //     { path: 'reservas', children: [
+  //       { path: '', component: MinhasReservasComponent},
+  //       { path: ':id', component: MinhaSacolaComponent}
+  //     ] },
+  //     {
+  //       path: 'sacolas', children: [
+  //         { path: '', component: SacolasComponent },
+  //         { path: ':id', component: MinhaSacolaComponent }
+  //       ]
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: 'atendente', canActivate: [CheckLogged], children: [
+  //     { path: '', component: HomeSupervisorComponent },
+  //     {
+  //       path: 'sacolas', children: [
+  //         { path: '', component: SacolasComponent },
+  //         { path: ':id', component: MinhaSacolaComponent }
+  //       ]
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: 'supervisor', canActivate: [CheckLogged], children: [
+  //     { path: '', component: HomeSupervisorComponent },
+  //     {
+  //       path: 'sacolas', children: [
+  //         { path: '', component: SacolasComponent },
+  //         { path: ':id', component: MinhaSacolaComponent }
+  //       ]
+  //     }
+  //   ]
+  // }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
