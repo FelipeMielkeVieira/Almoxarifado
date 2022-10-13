a589b67a85fe2c55f72140385fdc9065a4ec9fee
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-10-13 20:54:18 -0300

    13/10 - 20:54
    Added Maven

---
+++ Back-End-Maven/almoxarifado/src/test/java/br/senai/sc/almoxarifado/AlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sc.almoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
