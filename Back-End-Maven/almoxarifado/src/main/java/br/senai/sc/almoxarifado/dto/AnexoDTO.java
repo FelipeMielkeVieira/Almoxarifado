226b82888359ff6e535f18512f38d7d4525b4992
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-13 21:51:28 -0300

    13/10 - 21:50
    Implementando spring boot na classificacao e anexo.

--- Back-End-Maven/almoxarifado/src/main/java/br/senai/sc/almoxarifado/dto/AnexoDTO.java
+++ Back-End-Maven/almoxarifado/src/main/java/br/senai/sc/almoxarifado/dto/AnexoDTO.java
@@ -1,4 +1,17 @@
 package br.senai.sc.almoxarifado.dto;
 
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
 public class AnexoDTO {
+    @NotBlank
+    private Integer id;
+
+    @NotBlank
+    private Byte anexo;
+
+    @NotBlank
+    private boolean visibilidade;
 }
