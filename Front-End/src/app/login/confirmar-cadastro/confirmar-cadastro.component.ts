57837ba0e4045b9a5d3e3097cc8ce367b8c0253d
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-18 16:52:37 -0300

    18/10 - 16:52
    Começando a edição no componente confirmar-cadastro

--- Front-End/src/app/login/confirmar-cadastro/confirmar-cadastro.component.ts
+++ Front-End/src/app/login/confirmar-cadastro/confirmar-cadastro.component.ts
@@ -80,6 +80,7 @@ export class ConfirmarCadastroComponent implements OnInit {
         this.codigoVerificacaoIncorreto = false;
       }, 5000);
     }
+
   }
 
   // Função para criar o timer na segunda etapa da redefinição
@@ -144,17 +145,46 @@ export class ConfirmarCadastroComponent implements OnInit {
     this.fecharModal.emit('Finalizado');
   }
 
+  // Função para abrir os modais de alertas de funções efetuadas, criando um timeout de 5 segundos para desaparecer
+  // Recebe como parâmetro o número / id do modal que deverá ser aberto
+  // Os timeouts são armazenados numa lista
+  abrirModalAlerta(modal: number) {
+    switch (modal) {
+      case 1:
+        this.emailInvalido = true;
+        this.timeoutsAlertas[0] = setTimeout(() => {
+          this.emailInvalido = false;
+        }, 5000);
+        break;
+      case 2:
+        this.codigoVerificacaoIncorreto = true;
+        this.timeoutsAlertas[1] = setTimeout(() => {
+          this.codigoVerificacaoIncorreto = false;
+        }, 5000);
+        break;
+      case 3:
+        this.senhasNaoConferem = true;
+        this.timeoutsAlertas[2] = setTimeout(() => {
+          this.senhasNaoConferem = false;
+        }, 5000);
+        break;
+    }
+  }
+
   // Função para fechar os modais de alerta (alertaFeito)
   fecharModalAlerta(modal: number) {
     switch (modal) {
       case 1:
         this.emailInvalido = false;
+        clearTimeout(this.timeoutsAlertas[0]);
         break;
       case 2:
         this.codigoVerificacaoIncorreto = false;
+        clearTimeout(this.timeoutsAlertas[1]);
         break;
       case 3:
         this.senhasNaoConferem = false;
+        clearTimeout(this.timeoutsAlertas[2]);
         break;
     }
   }
