50223093f51c1edc908eef9cc30201434b5b15f7
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-10-17 21:50:06 -0300

    17/10 - 21:49
    Tirando os notblank e criando as rotas no postman

--- Back-End-Maven/almoxarifado/src/main/java/br/senai/sc/almoxarifado/dto/ClassificacaoDTO.java
+++ Back-End-Maven/almoxarifado/src/main/java/br/senai/sc/almoxarifado/dto/ClassificacaoDTO.java
@@ -1,14 +1,14 @@
 package br.senai.sc.almoxarifado.dto;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@ToString
 public class ClassificacaoDTO {
-    @NotBlank
     private Integer id;
 
-    @NotBlank
     private String classificacao;
 }
