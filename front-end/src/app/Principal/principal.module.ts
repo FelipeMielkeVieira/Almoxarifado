3202d8de7d3cf610df5eea2eceea1c68ef019bfe
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-10-26 21:45:52 -0300

    26/10 - 21:45

--- front-end/src/app/Principal/principal.module.ts
+++ front-end/src/app/Principal/principal.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './header/header.component';
 import { ModalConfiguracoesComponent } from './modal-configuracoes/modal-configuracoes.component';
 import { ModalEditarUserComponent } from './modal-editar-user/modal-editar-user.component';
 import { ModalUserComponent } from './modal-user/modal-user.component';
-import { ModalFiltragemItens } from './modal-filtragem-itens/modal-filtragem-itens.component';
 import { ModalOrdernar } from './modal-ordenar/modal-ordenar.component';
 import { ItemComponent } from './item/item.component';
 import { ModalReservarItem } from './modal-reservar-item/modal-reservar-item.component';
@@ -32,7 +31,6 @@ import {MatDividerModule} from '@angular/material/divider';
     ModalConfiguracoesComponent,
     ModalEditarUserComponent,
     ModalUserComponent,
-    ModalFiltragemItens,
     ModalOrdernar,
     ItemComponent,
     ModalReservarItem,
@@ -58,7 +56,6 @@ import {MatDividerModule} from '@angular/material/divider';
     ModalEditarUserComponent,
     ModalConfiguracoesComponent,
     ModalUserComponent,
-    ModalFiltragemItens,
     ModalOrdernar,
     ItemComponent,
     ModalReservarItem,
