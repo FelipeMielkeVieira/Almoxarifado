6b8a2de0b4885da347804dc29dc397e5351a560b
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-10-28 13:47:00 -0300

    28/10 - 13:46
    terminado cadastro de item

--- Front-End/src/app/gerencia/cadastrar-descricao-item/cadastrar-descricao-item.component.ts
+++ Front-End/src/app/gerencia/cadastrar-descricao-item/cadastrar-descricao-item.component.ts
@@ -37,53 +37,12 @@ export class CadastrarDescricaoItemComponent implements OnInit {
   // Função adicionada quando o usuário adiciona um arquivo no input de anexo, adiciona uma nova linha na tabela de anexos 
   anexoAdicionado() {
     let inputFile = document.getElementById('anexoInput') as HTMLInputElement;
-    const tbody = document.getElementById('tbodyAnexos');
-    const linha = document.createElement('tr');
-    const colunaNome = document.createElement('td');
-    const colunaDeletar = document.createElement('td');
-    const spanDeletar = document.createElement('span');
-
-    spanDeletar.classList.add('material-symbols-outlined');
-    spanDeletar.classList.add('icone');
-    spanDeletar.title = "Remover anexo";
-    spanDeletar.onclick = ($event) => {
-      this.removerAnexo($event);
-    };
-    spanDeletar.innerHTML = "delete";
 
     this.inputFileList.push.apply(this.inputFileList, inputFile.files);
-    // this.inputFileList.push(inputFile.files)
-    
-    colunaNome.innerHTML = inputFile.files![inputFile.files!.length - 1].name;
-
-    console.log(this.inputFileList);
-    console.log(inputFile.files);
-
-    linha.id = (this.inputFileList.length - 1).toString();
-    linha.className = "linha-anexo";
-
-    colunaDeletar.appendChild(spanDeletar);
-
-    colunaNome.className = 'itemTabela';
-    colunaDeletar.style.textAlign = "center";
-    colunaDeletar.classList.add('itemTabela');
-
-    linha.appendChild(colunaNome);
-    linha.appendChild(colunaDeletar);
-    tbody?.appendChild(linha);
   }
 
   // Remove um anexo da tabela de anexos e também do array de arquivos
-  //Falta lógica para excluir o arquivo da lista inputFileList
-  removerAnexo(event: any) {
-    console.log(event);
-    console.log(this.inputFileList);
-
-    const listaTr = document.querySelectorAll(".linha-anexo");
-
-    this.inputFileList.splice(parseInt(event.target.parentElement.parentElement.id), 1);
-    event.target.parentElement.parentElement.remove();
-
-    console.log(this.inputFileList);
+  removerAnexo(index: Number) {
+    this.inputFileList.splice(index, 1);
   }
 }
