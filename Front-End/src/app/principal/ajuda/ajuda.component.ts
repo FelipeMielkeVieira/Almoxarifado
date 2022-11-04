d8ec30d1dbf9d75cf27961de09096f5395a8a6cc
ThiagoBrag <thiago_braga@estudante.sesisenai.org.br>
2022-11-04 16:57:52 -0300

    16:57 - 04/11
    Ainda fazendo a tela de ajuda...

--- Front-End/src/app/principal/ajuda/ajuda.component.ts
+++ Front-End/src/app/principal/ajuda/ajuda.component.ts
@@ -23,4 +23,45 @@ export class AjudaComponent implements OnInit {
     }
   }
 
+  fecharCard(card: number) {
+    let cardElement: any = null;
+    let expandirElement: any = null;
+    let headerElement: any = null;
+    let conteudoElement: any = null;
+    switch(card) {
+      case 1:
+        cardElement = document.getElementById('card1');
+        expandirElement = document.getElementById('expandir1');
+        headerElement = document.getElementById('header1');
+        conteudoElement = document.getElementById('conteudo1');
+        break;
+      case 2:
+        cardElement = document.getElementById('card2');
+        expandirElement = document.getElementById('expandir2');
+        headerElement = document.getElementById('header2');
+        conteudoElement = document.getElementById('conteudo2');
+        break;
+      case 3:
+        cardElement = document.getElementById('card3');
+        expandirElement = document.getElementById('expandir3');
+        headerElement = document.getElementById('header3');
+        conteudoElement = document.getElementById('conteudo3');
+        break;
+    }
+    if (cardElement.style.display == 'block' ) {
+      expandirElement.style.transition = 'transform 1s';
+      expandirElement.style.transform = 'rotate(360deg)';
+      cardElement.style.animation = "subir 1.5s"
+      conteudoElement.style.display = 'none';
+      setTimeout(() => {
+        cardElement.style.display = 'none';
+      },1500);
+
+    } else {
+      expandirElement.style.transform = 'rotate(-180deg)';
+      expandirElement.style.transition = 'transform 1s';
+      cardElement.style.display = 'block';
+      cardElement.style.animation = "descer 1s"
+    }
+  }
 }
