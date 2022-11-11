90ca5dc83a5b64caf6193b5a0e4e89b3bb26dbe4
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-11-11 13:29:44 -0300

    11/11 - 13:29
    Busca de itens iniciais funcionando

--- Back-End-Maven/almoxarifado/src/main/java/br/senai/sc/almoxarifado/repository/ProdutoRepository.java
+++ Back-End-Maven/almoxarifado/src/main/java/br/senai/sc/almoxarifado/repository/ProdutoRepository.java
@@ -19,19 +19,51 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     List<Produto> findByClassificacao(Classificacao classificacao);
 
-    Page<Produto> findByVisibilidadeAndNomeContaining(Boolean visibilidade, String nome, Pageable pageable);
+    List<Produto> findByVisibilidadeAndNomeContaining(Boolean visibilidade, String nome, Pageable pageable);
 
     Integer countByVisibilidadeAndNomeContaining(Boolean b, String nome);
+    
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeAndDescartavelAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, Boolean b1, String nome, Pageable pageable);
 
-//    List<Produto> findByVisibilidadeAndClassificacaoAndEstoqueAndDescartavelAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, Boolean b1, String nome, Pageable pageable);
-//
-//    List<Produto> findByVisibilidadeAndClassificacaoAndDescartavelAndNomeContaining(Boolean b, Classificacao classificacao1, Boolean b1, String nome, Pageable pageable);
-//
-//    List<Produto> findByVisibilidadeAndClassificacaoAndEstoqueIsNotAndDescartavelAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, Boolean b1, String nome, Pageable pageable);
-//
-//    List<Produto> findByVisibilidadeAndClassificacaoAndEstoqueAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, String nome, Pageable pageable);
-//
-//    List<Produto> findByVisibilidadeAndClassificacaoAndNomeContaining(Boolean b, Classificacao classificacao1, String nome, Pageable pageable);
-//
-//    List<Produto> findByVisibilidadeAndClassificacaoAndEstoqueIsNotAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, String nome, Pageable pageable);
+    List<Produto> findByVisibilidadeAndClassificacaoAndDescartavelAndNomeContaining(Boolean b, Classificacao classificacao1, Boolean b1, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeIsNotAndDescartavelAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, Boolean b1, String nome, Pageable pageable);
+    
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndNomeContaining(Boolean b, Classificacao classificacao1, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeIsNotAndNomeContaining(Boolean b, Classificacao classificacao1, Integer i, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeAndDescartavelAndNomeContaining(Boolean b, Integer i, Boolean b1, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeAndNomeContaining(Boolean b, Integer i, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndDescartavelAndNomeContaining(Boolean b, Boolean b1, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeIsNotAndDescartavelAndNomeContaining(Boolean b, Integer i, Boolean b1, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeIsNotAndNomeContaining(Boolean b, Integer i, String nome, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeAndDescartavel(Boolean b, Classificacao classificacao1, Integer i, Boolean b1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidade(Boolean b, Classificacao classificacao1, Integer i, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndDescartavel(Boolean b, Classificacao classificacao1, Boolean b1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacao(Boolean b, Classificacao classificacao1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeIsNotAndDescartavel(Boolean b, Classificacao classificacao1, Integer i, Boolean b1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndClassificacaoAndQuantidadeIsNot(Boolean b, Classificacao classificacao1, Integer i, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeAndDescartavel(Boolean b, Integer i, Boolean b1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidade(Boolean b, Integer i, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndDescartavel(Boolean b, Boolean b1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeIsNotAndDescartavel(Boolean b, Integer i, Boolean b1, Pageable pageable);
+
+    List<Produto> findByVisibilidadeAndQuantidadeIsNot(Boolean b, Integer i, Pageable pageable);
 }
