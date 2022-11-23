4c94cad80afe57ecd6eecb223ec1334be02444f6
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2022-11-22 21:56:57 -0300

    22/11 - 21:56
    Arrrumando projeto

--- Back-End/src/br/senai/sc/almoxarifado/model/dao/ProdutoReservaDAO.java
+++
@@ -1,4 +0,0 @@
-package br.senai.sc.almoxarifado.model.dao;
-
-public class ProdutoReservaDAO {
-}
--- Back-End/src/br/senai/sc/almoxarifado/model/entities/Atendente1.java
+++
@@ -1,9 +0,0 @@
-package br.senai.sc.almoxarifado.model.entities;
-
-public class Atendente1 extends Usuario {
-    public Atendente1() { }
-
-    public Atendente1(String emailUsuario, String senhaUsuario, String nomeUsuario) {
-        super(emailUsuario, senhaUsuario, nomeUsuario);
-    }
-}
